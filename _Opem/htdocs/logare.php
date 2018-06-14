b9695e7fe3caf76f7581d065a622bfd72a3a0e38
RobertAbabei <robert_ababei97@yahoo.com>
2018-06-14 03:38:55 +0300

    Add files via upload

--- _Opem/htdocs/logare.php
+++ _Opem/htdocs/logare.php
@@ -5,25 +5,14 @@
 	
 	<div id="login_form">
 
-        <?php
-            if (isset($_SESSION['u_id'])){
-                echo '<div style="background-color:white;">V-ati autentificat cu succes.<br>Bine ai venit, '. $_SESSION['u_uid'] . '.<br>'
-                        .$_SESSION['u_first'] . '.<br>'
-                        .$_SESSION['u_last'] . '.<br>'
-                        .$_SESSION['u_email'] . '.<br>'
-                        .$_SESSION['u_id'] .'</div>';
-                
-                echo '<form action="includes/logout.php" method="POST">
-                            <input type="submit" name="submit" value="Delogare" style="font-size:18px;">
-                        </form>';
-            } else {
-                echo '<form name="f1" method="post" action="includes/login.php" id="f1">
-                 <table>
+        
+                <form name="f1" method="post" action="includes/login.php" id="f1">
+                 <table class="login_table" >
                     <tr>
                         <td class="f1_label">Utilizatoriii:</td><td><input class="input_log" type="text" name="uid" value="" />
                         </td>
                     </tr>
-                    <tr>
+                    <tr id="make-yellow">
                         <td class="f1_label">Parola:</td><td><input class="input_log" type="password" name="pwd" value=""  />
                         </td>
                     </tr>
@@ -36,18 +25,9 @@
                         </td>
                     </tr>
                 </table>
-            </form>';
-            }
-            if (isset($_GET['login'])){
-         if($_GET['login']=='empty')
-           echo"<h3 class='eroare'>Campuri necompletate</h3>";
-            else if($_GET['login']=='ue')
-            echo"<h3 class='eroare'>Nume/email gresit</h3>";
-             else if($_GET['login']=='pass')
-              echo"<h3 class='eroare'>Parola incorecta</h3>";       
-}
-        ?> 
-
+            </form>
+            
+        
 </div>
 
 
